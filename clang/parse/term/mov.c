1c5b503981589d445f43876579a16b7546d00194
Seu Nome <seu@email.com>
2026-01-05 19:43:57 -0300

    Fix MOV node by desugaring into cloned LET using DUP semantics

--- clang/parse/term/mov.c
+++ clang/parse/term/mov.c
@@ -9,11 +9,12 @@ fn Term parse_term_mov(PState *s, u32 depth) {
   parse_skip(s);
   parse_match(s, ";");
   parse_skip(s);
-  parse_bind_push(nam, depth, 0, PBIND_MOV, 0);
+  parse_bind_push(nam, depth, 0, PBIND_LAM, 1);
+  u64  loc  = heap_alloc(1);
   Term body = parse_term(s, depth + 1);
+  body = parse_auto_dup(body, depth + 1, depth + 1, BJV, 0);
+  HEAP[loc] = body;
   parse_bind_pop();
-  u64 loc = heap_alloc(2);
-  HEAP[loc + 0] = val;
-  HEAP[loc + 1] = body;
-  return term_new(0, MOV, 0, loc);
+  Term lam = term_new(0, LAM, depth + 1, loc);
+  return term_new_app(lam, val);
 }
